1007e12a1010c300f49f6e93347a51be4ea03559
Antoine <alexander.deng@mail.mcgill.ca>
2022-11-22 15:19:03 -0500

    cm

--- FNN.py
+++ FNN.py
@@ -3,6 +3,9 @@ import pandas as pd
 from numpy import newaxis
 import time
 import csv
+from sklearn.metrics import accuracy_score
+import sklearn
+import matplotlib.pyplot as plt
 
 #save hidden parameters of a network into txt file
 def save_net(filename, net):
@@ -256,7 +259,7 @@ def accuracy(prediction, actual):
     return np.sum(prediction == actual)/size
 
 #loading training data
-data = pd.read_csv('digit-recognizer/train.csv')
+data = pd.read_csv('test.csv')
 data.head()     
 data = np.array(data)
 global m,n
@@ -293,9 +296,9 @@ net.add(ActivationLayer(sigmoid, sigmoid_prime))
 t1 = time.time()
 #net.bgd(X_train[0:1000], Y_train[0:1000], epochs=4000, alpha=0.1)
 #net.sgd(X_train, Y_train, epochs=11, alpha=0.1)
-net.mini_bgd(X_train, Y_train, epochs=10, alpha=0.1)
+#net.mini_bgd(X_train, Y_train, epochs=10, alpha=0.1)
 #save_net('net_minibgd_100_3sigmoid_50.txt', net)
-#net = load_net('net_minibgd_100_3sigmoid_50.txt')
+net = load_net('net_minibgd_100_3sigmoid_50.txt')
 print("time taken: ", time.time()-t1)
 
 #testing
@@ -308,3 +311,5 @@ x_test = (data[0:data.shape[0]-1].T)
 print("test x,y shapes: ", x_test.shape, y_test.shape)
 out2 = net.predict(x_test)
 print("accuracy: ", accuracy(out2, y_test))
+sklearn.metrics.ConfusionMatrixDisplay.from_predictions(y_test, out2)
+plt.show()
